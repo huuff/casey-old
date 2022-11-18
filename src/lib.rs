ac3fd72c84f13846dcc967b9907b7ff0fdc52fd2
Haf <haf@protonmail.ch>
2022-11-18 17:31:05 +0100

    validation module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod normalize;
 pub mod convert;
 pub mod args;
 pub mod text_detect;
+pub mod validation;
 
 use crate::args::{Args, Command};
 use crate::case::Case;
@@ -12,27 +13,7 @@ use std::io::{self, Write, Error};
 use crate::text_detect::text_detect;
 use std::process;
 use std::fs;
-
-fn check_inline(input: &str) {
-    if input.chars().any(|c| c.is_whitespace()) {
-        println!("Sorry, the --inline option only accepts single tokens with no whitespace");
-        process::exit(1);
-    }
-}
-
-fn check_ascii(input: &str) {
-    if input.chars().any(|c| !c.is_ascii()) {
-        println!("Sorry, this program only accepts ASCII");
-        process::exit(1);
-    }
-}
-
-fn check_no_from(from: Option<String>) {
-    if from.is_some() {
-        println!("The --from flag is not allowed for this operation");
-        process::exit(1);
-    }
-}
+use crate::validation::{check_inline, check_ascii, check_no_from};
 
 fn detect_and_collect_report(input: &str, verbose: bool, output: &mut impl Write) -> Result<usize, Error> {
     check_ascii(&input);
---
+++ src/validation.rs
@@ -0,0 +1,23 @@
+use std::process;
+
+
+pub fn check_inline(input: &str) {
+    if input.chars().any(|c| c.is_whitespace()) {
+        eprintln!("Sorry, the --inline option only accepts single tokens with no whitespace");
+        process::exit(1);
+    }
+}
+
+pub fn check_ascii(input: &str) {
+    if input.chars().any(|c| !c.is_ascii()) {
+        eprintln!("Sorry, this program only accepts ASCII");
+        process::exit(1);
+    }
+}
+
+pub fn check_no_from(from: Option<String>) {
+    if from.is_some() {
+        eprintln!("The --from flag is not allowed for this operation");
+        process::exit(1);
+    }
+}
